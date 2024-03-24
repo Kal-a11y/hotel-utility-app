3b1effae3d420999987280216b14ac9c80ec2adf
Kaila <kailaamia@gmail.com>
2024-03-23 23:53:29 -0500

    Added method to location model to update item counts

--- server/models/Location.js
+++ server/models/Location.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require('mongoose');
+const Item = require('./Item');
  
 const locationSchema = new Schema({
     name: {
@@ -17,5 +18,49 @@ const locationSchema = new Schema({
     nextCount: Date,
 });
 
+locationSchema.methods.updateTotalCount = async function () {
+    try {
+        const totalStock = await Location.aggregate([ 
+            {
+                $unwind: "$stock"
+            },
+            {
+                $group: {
+                    _id: "$stock.item",
+                    totalCount: { $sum: "$stock.count" }
+                }
+            },
+            {
+                $lookup: {
+                    from: "items",
+                    localField: "_id",
+                    foreignField: "_id",
+                    as: "item"
+                }
+            },
+            {
+                $project: {
+                    _id: 0,
+                    itemId: "$item._id",
+                    totalCount: 1
+                }
+            }
+        ]);
+       
+
+        for (const item of totalStock) {
+            const { itemId, totalCount } = item;
+            const updatedItem = await Item.findByIdAndUpdate(itemId, { totalCount }, { new: true });
+            // console.log(updatedItem);
+        }
+        return totalStock;
+    } catch (error) {
+        console.error(error);
+    }
+
+}
+
+
 const Location = model('location', locationSchema);
+
 module.exports = Location;
