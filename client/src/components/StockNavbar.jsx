3dd032b14e79db6edcd200c53514b0e9f2155f84
Kaila <kailaamia@gmail.com>
2024-03-27 17:26:53 -0500

    Added links to stock nav buttons

--- client/src/components/StockNavbar.jsx
+++ client/src/components/StockNavbar.jsx
@@ -3,12 +3,13 @@ const StockNavbar = () => {
     const { stockCategory } = useParams();
     return (
         <>
-            <button>Locations</button>
-            <button>Items</button>
-            {stockCategory === 'items' ? (
-                <button>Add Item</button>
-            ) : (
-                <button>Add Location</button>
+            <button onClick={() => navigate('/stock/locations')}>Locations</button>
+            <button onClick={() => navigate('/stock/items')}>Items</button>
+            {location.pathname == '/stock/items' && (
+                <button onClick={() => navigate('/stock/addItem')}>Add Item</button>
+            ) }
+            {location.pathname == '/stock/locations' &&(
+                <button onClick={() => navigate('/stock/addLocation')}>Add Location</button>
             )}
 
         </>
