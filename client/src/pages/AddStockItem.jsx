289933d504fa083dba131d74e6a385fb4973533e
Kaila <kailaamia@gmail.com>
2024-04-01 08:06:16 -0500

    Added handleInput change and itemFormState

--- client/src/pages/AddStockItem.jsx
+++ client/src/pages/AddStockItem.jsx
@@ -1,16 +1,22 @@
 import { ADD_ITEM } from "../utils/API";
 import { useState } from "react";
 const AddStockItem = () => {
+    const [itemFormState, setItemFormState] = useState({name: '', countBy: '', minCount: ''});
+   
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        setItemFormState({ ...itemFormState, [name]: value });
+    };
     return (
         <div>
             <h1>Add Stock Item</h1>
             <form>
                 <label htmlFor="name">Name:</label>
-                <input type="text" id="name" name="name" />
+                <input type="text" id="name" name="name" onChange={handleInputChange} />
                 <label htmlFor="countBy">Count By:</label>
-                <input type="text" id="countBy" name="countBy" />
+                <input type="text" id="countBy" name="countBy" onChange={handleInputChange} />
                 <label htmlFor="minCount">Minimum Count:</label>
-                <input type="number" id="minCount" name="minCount" />
+                <input type="number" id="minCount" name="minCount" onChange={handleInputChange} />
                 <button type="submit">Add Item</button>
             </form>
         </div>
